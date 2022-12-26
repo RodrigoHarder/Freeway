7d2d1242c7fa641bbc11e175f5c763d9235b6981
Rodrigo Harder <114362538+RodrigoHarder@users.noreply.github.com>
2022-12-26 08:48:17 -0300

    Adicionando arquivos que fazem parte do jogo

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 400);
+  somTrilha.loop();
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  movimentaAtor();
+  mostraCarro();
+  movimentaCarro();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluirPontos();
+  marcaPonto();
+}
